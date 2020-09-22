1fcae349898c447a5e6eae9c8f07923a5573770b
shunkitatani <shun19990502@icloud.com>
2020-09-22 18:44:46 +0900

    ログイン処理まで

--- chatapplication/chat/urls.py
+++ chatapplication/chat/urls.py
@@ -1,9 +1,12 @@
 
 from django.contrib import admin
 from django.urls import path, include
-from .views import index, room
+from .views import index, room, signupfunc, loginfunc, logoutfunc
 
 urlpatterns = [
+    path('signup/', signupfunc, name='signup' ),
+    path('login/', loginfunc, name='login'),
+    path('logout/', logoutfunc, name='logout'),
     path('index/', index, name='index'),
     path('room/<int:pk>', room, name='room'),
 ]
--- chatapplication/chat/views.py
+++ chatapplication/chat/views.py
@@ -1,7 +1,42 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 from .forms import ProfileForm
 from .models import Profile
 # Create your views here.
+
+def signupfunc(request):
+    if request.method == 'POST':
+        username2 = request.POST['username']
+        mailadress2 = request.POST['mailadress']
+        password2 =  request.POST['password']
+        try:
+            User.objects.get(username=username2)
+            return render(request, 'signup.html', {'error':'このユーザーは登録されています'})
+        except:
+            user = User.objects.create_user(username2, mailadress2, password2)
+            return redirect('login')
+    return render(request, 'signup.html')
+
+
+def loginfunc(request):
+    if request.method == 'POST':
+        username2 = request.POST['username']
+        mailadress2 = request.POST['mailadress']
+        password2 =  request.POST['password']
+        user = authenticate(request, username=username2, mailadress=mailadress2, password=password2)
+        if user is not None:
+            login(request, user)
+            return redirect('index')
+        else:
+            return render(request, 'login.html', {'loginerror':'ログイン情報が間違っています'})
+    return render(request, 'login.html')
+
+def logoutfunc(request):
+    logout(request)
+    return redirect('login')
+
 def index(request):
     if request.method == 'POST':
         obj = Profile()
